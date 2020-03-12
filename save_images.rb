e018a19c49c804a030596c0c27ee7347e412c9e2
Yuya Fujiwara <asonas@cookpad.com>
2020-03-12 19:55:10 +0900

    refactor

--- save_images.rb
+++ save_images.rb
@@ -2,53 +2,50 @@ require 'twitter'
 require 'pp'
 require 'pry'
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key    = ENV['TWITTER_API_KEY']
-  config.consumer_secret = ENV['TWITTER_API_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-end
-
-
 class SaveTheWani
   AUTHOR = "yuukikikuchi"
 
   def initialize
     @collection = []
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key    = ENV['TWITTER_API_KEY']
+      config.consumer_secret = ENV['TWITTER_API_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+    end
   end
 
   def self.run
     new.search
+    p @collection.map(&:media_url)
   end
 
   def search
-    client.user_timeline(AUTHOR, count:300).take(1).each do |tweet|
+    @client.user_timeline(AUTHOR, count:300).take(1).each do |tweet|
       next unless tweet.text.match? /日目/
       next if tweet.text.match? /(RT|から)/
 
       t = SaveTheWani::Tweet.new(
         id: tweet.id,
         text: tweet.text,
-        media_url: tweet.media_url
+        media_url: tweet.media.first.media_url
       )
+      binding.pry
       @collection.push t
     end
   end
 
   class Tweet
-    attr_accessor: :id, :text, :media_url
+    attr_accessor :id, :text, :media_url
+
+    def initialize(id:, text:, media_url:)
+      @id = id
+      @text = text
+      @media_url = media_url
+    end
   end
 end
 
-target_user = "yuukikikuchi"
+SaveTheWani.run
 
-collection = []
-client.user_timeline(target_user, count:300).take(1).each do |tweet|
-  next unless tweet.text.match? /日目/
-  next if tweet.text.match? /(RT|から)/
-  binding.pry
-  wani_tweet = SaveTheWani::Tweet.new id: tweet.id, text: tweet.text, media_url: tweet.media_url
-  collection.push wani_tweet
-end
 
-collection
